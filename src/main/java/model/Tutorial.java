c5941812063c32b2afdab38e79f5e457d0ef36ee
ewgenij87snwork <ewgenij87snwork@gmail.com>
2024-04-20 12:49:05 +0300

    Git init

---
+++ src/main/java/model/Tutorial.java
@@ -0,0 +1,23 @@
+package model;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+    public Tutorial() {}
+}
